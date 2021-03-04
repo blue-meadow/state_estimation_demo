b11e84e2419e5946d22efa51bddc24372ad470fd
Izzy Brand <izzybrand@gmail.com>
2021-03-04 01:25:48 -0500

    add observation covariance

--- observation.py
+++ observation.py
@@ -1,18 +1,37 @@
 import numpy as np
+from scipy.stats import multivariate_normal
+
 from dynamics import simple_2d_dynamics
 
-def simple_2d_acc_gyro_mag(state, command):
-	acc_noise_scale = 0.5  # m/s/s
-	gyro_noise_scale = 0.1  # radians/s
-	mag_noise_scale = 0.1  # radians/s
 
-	# we will need the accelerations as well
-	statedot = simple_2d_dynamics(state, command)
+# accx, accy, gyro, mag
+simple_2d_observation_cov = np.diag([0.5, 0.5, 0.1, 0.1,])
 
-	# compute simulated observations
-	acc = state_dot[3:5] + np.random.randn(2) * acc_noise_scale
-	gyro = state_dot[5] + np.random.randn(1) * gyro_noise_scale
-	mag = state[0:2] + np.random.randn(2) * mag_noise_scale
+# for simulation
+def simple_2d_acc_gyro_mag_sample(state, command):
+    # we will need the accelerations as well
+    statedot = simple_2d_dynamics(state, command)
+    # compute simulated observations
+    accx = statedot[3]
+    accy = statedot[4]
+    gyro = statedot[5]
+    mag = state[2]
+    observation = np.array([accx, accy, gyro, mag])
+    # and additive noise
+    noise = multivariate_normal.rvs(cov=simple_2d_observation_cov)
+    return observation + noise
 
-	# return the vector of observations
-	return np.concatentate([acc, gyro, mag])
+# for state estimation
+def simple_2d_acc_gyro_mag_likelihood(state, command, observation):
+    # we will need the accelerations as well
+    statedot = simple_2d_dynamics(state, command)
+    # compute expected observations
+    accx = statedot[3]
+    accy = statedot[4]
+    gyro = statedot[5]
+    mag = state[2]
+    expected_observation = np.array([accx, accy, gyro, mag])
+    # and use that to find the likelihood of the given observation
+    likelihood =  multivariate_normal.pdf(x=observation,
+        mean=expected_observation, cov=simple_2d_observation_cov)
+    return likelihood
