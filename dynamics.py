725e6510fb83573f05dde0e2aa83897871af7fe8
Izzy Brand <izzybrand@gmail.com>
2021-03-04 01:25:06 -0500

    fix use of concatenate

--- dynamics.py
+++ dynamics.py
@@ -1,14 +1,18 @@
 import numpy as np
 
+
+# x, y, theta, xdot, ydot, thetadot
+simple_2d_transition_cov = np.diag([0.01, 0.01, 0.005, 0.001, 0.001, 0.0005])
+
 def simple_2d_dynamics(state, command):
-	# break out the vehicle state and command
-	x, y, theta, xdot, ydot, thetadot = state
-	l_thrust, r_thrust = command
+    # break out the vehicle state and command
+    x, y, theta, xdot, ydot, thetadot = state
+    l_thrust, r_thrust = command
 
-	# compute the accelerations
-	xddot = np.cos(theta) * (r_thrust + l_thrust)
-	xddot = np.sin(theta) * (r_thrust + l_thrust)
-	thetaddot = r_thrust - l_thrust
+    # compute the accelerations
+    xddot = np.cos(theta) * (r_thrust + l_thrust)
+    yddot = np.sin(theta) * (r_thrust + l_thrust)
+    thetaddot = r_thrust - l_thrust
 
-	# and return the time derivative of the state
-	return np.array([xdot, ydot, thetadot, xddot, yddot, thetaddot])
+    # and return the time derivative of the state
+    return np.array([xdot, ydot, thetadot, xddot, yddot, thetaddot])
